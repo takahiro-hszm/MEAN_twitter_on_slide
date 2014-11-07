8cb53af2b132d2e29c0549a8481aa83081d4956a
Iwami kazuya <k.bluewing.i@gmail.com>
2014-11-07 23:42:44 +0900

    mainを一旦全消し

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,61 +2,66 @@
 
 angular.module('tweetOnSlideApp')
   .controller('MainCtrl', function ($scope, $http, socket) {
-    var obj_2014 = {
-      "base":{
-        color:"white",
-        speed:10000,
-        interval:700,
-        font_size:"50px",
-        loop:false
-      },
-      "comments":[
-      ]
-    }; //speedとintervalの指定方法変えたからnicoscreenの方見て
-    nicoscreen.set(obj_2014);
-    nicoscreen.start();
-
-    $('.flexslider').flexslider({
-      slideshow: false
-    });
-
-    var elem = document.getElementById('nicoscreen');
-    document.getElementById('nicoscreen').addEventListener('click', function () {
-      if (screenfull.enabled) {
-        screenfull.request(elem);
-      }
-    });
 
 
-    $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
-    });
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      nicoscreen.add($scope.newThing);
-      $scope.newThing = '';
 
-    };
+    // var obj_2014 = {
+    //   "base":{
+    //     color:"white",
+    //     speed:10000,
+    //     interval:700,
+    //     font_size:"50px",
+    //     loop:false
+    //   },
+    //   "comments":[
+    //   ]
+    // }; //speedとintervalの指定方法変えたからnicoscreenの方見て
+    // nicoscreen.set(obj_2014);
+    // nicoscreen.start();
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    // $('.flexslider').flexslider({
+    //   slideshow: false
+    // });
 
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
+    // var elem = document.getElementById('nicoscreen');
+    // document.getElementById('nicoscreen').addEventListener('click', function () {
+    //   if (screenfull.enabled) {
+    //     screenfull.request(elem);
+    //   }
+    // });
 
-    socket.on('eeic2014', function (data) {
 
-       $http.post('/api/things', { name: data });
-       nicoscreen.add(data);
-    });//画面遷移を繰り返すと何本もコネクション張ってる？？
+    // $scope.awesomeThings = [];
+
+    // $http.get('/api/things').success(function(awesomeThings) {
+    //   $scope.awesomeThings = awesomeThings;
+    //   socket.syncUpdates('thing', $scope.awesomeThings);
+    // });
+
+    // $scope.addThing = function() {
+    //   if($scope.newThing === '') {
+    //     return;
+    //   }
+    //   $http.post('/api/things', { name: $scope.newThing });
+    //   nicoscreen.add($scope.newThing);
+    //   $scope.newThing = '';
+
+    // };
+
+    // $scope.deleteThing = function(thing) {
+    //   $http.delete('/api/things/' + thing._id);
+    // };
+
+    // $scope.$on('$destroy', function () {
+    //   socket.unsyncUpdates('thing');
+    // });
+
+    // socket.on('eeic2014', function (data) {
+
+    //    $http.post('/api/things', { name: data });
+    //    nicoscreen.add(data);
+    // });//画面遷移を繰り返すと何本もコネクション張ってる？？
 
   });
